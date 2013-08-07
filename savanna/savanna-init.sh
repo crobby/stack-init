18c60ef16a4822b7fa58b9b0becdd88e659951ac
Chad Roberts <croberts@redhat.com>
2013-08-07 11:50:43 -0400

    Additional files and tweaks to the savanna-init script

--- savanna/savanna-init.sh
+++ savanna/savanna-init.sh
@@ -12,16 +12,23 @@ chmod +x jq
 export MASTER_ID=$(http http://localhost:18080/v1.0/$TENANT/node-group-templates X-Auth-Token:$TOKEN | ./jq ".node_group_templates[0].id")
 export WORKER_ID=$(http http://localhost:18080/v1.0/$TENANT/node-group-templates X-Auth-Token:$TOKEN | ./jq ".node_group_templates[1].id")
 
+rm jq
 
-sed -i "s/MASTER_ID/$MASTER_ID/g" cluster.tmp
-sed -i "s/WORKER_ID/$WORKER_ID/g" cluster.tmp
+cp ~/src/cluster.tmp ~/src/cluster.tmp.bak
+
+sed -i "s/MASTER_ID/$MASTER_ID/g" ~/src/cluster.tmp
+sed -i "s/WORKER_ID/$WORKER_ID/g" ~/src/cluster.tmp
 
 http http://localhost:18080/v1.0/$TENANT/cluster-templates X-Auth-Token:$TOKEN < ~/src/cluster.tmp
+
+cp ~/src/cluster.tmp.bak ~/src/cluster.tmp
+rm ~/src/cluster.tmp.bak
 
 #register our image
 #get the image id for our known image
 export AUTH_URL="http://192.168.1.44:5000/v2.0"
 
-export IMAGE_ID=$(nova --os-username=admin --os-tenant=admin --os-password=admin --os-auth-url=$AUTH_URL image-list | grep fedora-hdp.image | awk {'print $2'})
-http http://localhost:18080/v1.0/$TENANT/images/$IMAGE_ID X-Auth-Token:$TOKEN < image.reg
-http http://localhost:18080/v1.0/$TENANT/images/$IMAGE_ID/tag X-Auth-Token:$TOKEN < tags.reg
+export IMAGE_ID=$(nova --os-username=admin --os-tenant-name=admin --os-password=admin --os-auth-url=$AUTH_URL image-list | grep fedora-hdp.image | awk {'print $2'})
+http http://localhost:18080/v1.0/$TENANT/images/$IMAGE_ID X-Auth-Token:$TOKEN < ~/src/image.reg
+http http://localhost:18080/v1.0/$TENANT/images/$IMAGE_ID/tag X-Auth-Token:$TOKEN < ~/src/tags.reg
+
