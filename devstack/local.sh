0aef2f1e1822b9337fbadea4344bd5b1b3f5729f
Chad Roberts <croberts@redhat.com>
2014-09-05 10:58:32 -0400

    Updating local.sh with my latest stuff
    adding domain creation for sahara-proxy

--- devstack/local.sh
+++ devstack/local.sh
@@ -13,9 +13,20 @@ nova-manage flavor create c1.elevengig 1024 1 11 0 0 0
 nova-manage flavor create --name c.test --memory 2048 --cpu 1 --root_gb 17 --ephemeral_gb 0 --flavor 15 --swap 0 --is_public True
 nova-manage flavor create --name c.sixteengig --memory 2048 --cpu 1 --root_gb 17 --ephemeral_gb 0 --flavor 55 --swap 0 --is_public True
 # I happen to have an image that I always want loaded
-glance image-create --name=ubuntu-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-0.3-vanilla-1.2.1-ubuntu-13.04.qcow2
+#glance image-create --name=fedora-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/new-setenforce1-fedora_hadoop_1_1_2.qcow2
 
-glance image-create --name=fedora-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-icehouse-vanilla-1.2.1-fedora-19.qcow2
+#glance image-create --name=ubuntu-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-0.2-vanilla-1.1.2-oozie-3.3.2-ubuntu-13.10.qcow2
+#glance image-create --name=ubuntu-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-0.3-vanilla-1.2.1-ubuntu-13.04.qcow2
+
+glance image-create --name=ubuntu-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/ubuntu_sahara_vanilla_hadoop_1_latest.qcow2
+
+#glance image-create --name=fedora-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-0.3-vanilla-1.2.1-fedora-19.qcow2
+
+glance image-create --name=new-fedora-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/fedora_sahara_vanilla_hadoop_1_latest.qcow2
+
+#glance image-create --name=fedora-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-icehouse-vanilla-1.2.1-fedora-19.qcow2
+
+#glance image-create --name=rhel-server.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/rhel-server-x86_64-kvm-6.4_20130130.0-4.qcow2
 
 # I also open up the firewall on my local box that is running openstack (because I run dashboard/savanna from another box)
 sudo firewall-cmd --add-port=1-65535/tcp || echo "already all set"
@@ -27,3 +38,6 @@ nova --os-username=admin --os-tenant-name=admin --os-password=admin --os-auth-ur
 export OS_USERNAME=admin
 keystone service-create --name sahara --type data_processing --description "Sahara Data Processing"
 keystone endpoint-create --service sahara --region RegionOne --publicurl "http://127.0.0.1:18080/v1.1/\$(tenant_id)s" --adminurl "http://127.0.0.1:18080/v1.1/\$(tenant_id)s" --internalurl "http://127.0.0.1:18080/v1.1/\$(tenant_id)s"
+
+#create sahara-proxy domain
+~/devstack/domain.sh
