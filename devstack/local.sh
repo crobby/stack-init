94099da3acb415e43133069c8e9c270b24f86658
Chad Roberts <croberts@redhat.com>
2015-09-10 10:43:05 -0400

    Update local.sh

--- devstack/local.sh
+++ devstack/local.sh
@@ -1,43 +1,77 @@
-sudo setenforce 0
-
-# get our env params
-source ~/devstack/openrc
+source ~/src/devstack/openrc
 
 # open up our vms...a little wide here, but I'm ok with that, you may not be
-nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
-nova secgroup-add-rule default tcp 1 65535 0.0.0.0/0
+
+PRIVATE_NET_ID=$(neutron net-list | grep private | awk '{print $2}')
+PUBLIC_NET_ID=$(neutron net-list | grep public | awk '{print $2}')
+
+sudo iptables -t nat -A POSTROUTING -o enp0s25 -j MASQUERADE
+
+# the following 2 lines are a bit experimental...
+sudo iptables -A INPUT ACCEPT
+sudo iptables -A FORWARD ACCEPT
+
+
+neutron net-update $PRIVATE_NET_ID --shared True
+neutron net-update $PUBLIC_NET_ID --shared True
+neutron subnet-update private-subnet --dns_nameservers list=true 8.8.8.8 8.8.4.4
+
+source ~/src/devstack/openrc demo demo
+
+for group in $(neutron security-group-list | grep default | awk -F '|' '{ print $2 }')
+do
+    tenant_id=$(neutron security-group-show $group | grep tenant_id | awk '{print $4}' | tail -n 1)
+        neutron security-group-rule-create --protocol icmp --direction ingress $group
+        neutron security-group-rule-create --protocol icmp --direction egress $group
+        neutron security-group-rule-create --protocol tcp --port-range-min 1 --port-range-max 65535 --direction ingress $group
+        neutron security-group-rule-create --protocol tcp --port-range-min 1 --port-range-max 65535 --direction egress $group
+        neutron security-group-rule-create --protocol udp --port-range-min 1 --port-range-max 65535 --direction egress $group
+        neutron security-group-rule-create --protocol udp --port-range-min 1 --port-range-max 65535 --direction ingress $group
+        break
+done
+
+source ~/src/devstack/openrc admin admin
+
+for group in $(neutron security-group-list | grep default | awk -F '|' '{ print $2 }')
+do
+    tenant_id=$(neutron security-group-show $group | grep tenant_id | awk '{print $4}' | tail -n 1)
+        neutron security-group-rule-create --protocol icmp --direction ingress $group
+        neutron security-group-rule-create --protocol icmp --direction egress $group
+        neutron security-group-rule-create --protocol tcp --port-range-min 1 --port-range-max 65535 --direction ingress $group
+        neutron security-group-rule-create --protocol tcp --port-range-min 1 --port-range-max 65535 --direction egress $group
+        neutron security-group-rule-create --protocol udp --port-range-min 1 --port-range-max 65535 --direction egress $group
+        neutron security-group-rule-create --protocol udp --port-range-min 1 --port-range-max 65535 --direction ingress $group
+        break
+done
+
+
 
 # I want an 11 gig flavor  <name, memory, cpus, storage, ephem, swap, ID>  change ID if you want multiple flavors
-nova-manage flavor create c1.elevengig 1024 1 11 0 0 0
+openstack flavor create c.fivegig --id 56 --ram 2048 --disk 5 --vcpus 1
+openstack flavor create c.sixgig --id 55 --ram 2048 --disk 6 --vcpus 1
+openstack flavor create c.eight --id 59 --ram 2048 --disk 8 --vcpus 1
+openstack flavor create c.fourgig --id 53 --ram 2048 --disk 4 --vcpus 1
+openstack flavor create c.threegig --id 54 --ram 2048 --disk 3 --vcpus 1
+openstack flavor create c.cloudera --id 88 --ram 6144 --disk 9 --vcpus 1
+openstack flavor create c.spark --id 99 --ram 2048 --disk 9 --vcpus 1
 
-nova-manage flavor create --name c.test --memory 2048 --cpu 1 --root_gb 17 --ephemeral_gb 0 --flavor 15 --swap 0 --is_public True
-nova-manage flavor create --name c.sixteengig --memory 2048 --cpu 1 --root_gb 17 --ephemeral_gb 0 --flavor 55 --swap 0 --is_public True
-# I happen to have an image that I always want loaded
-#glance image-create --name=fedora-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/new-setenforce1-fedora_hadoop_1_1_2.qcow2
+#nova flavor-create c1.elevengig 0 1024 11 1
 
-#glance image-create --name=ubuntu-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-0.2-vanilla-1.1.2-oozie-3.3.2-ubuntu-13.10.qcow2
-#glance image-create --name=ubuntu-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-0.3-vanilla-1.2.1-ubuntu-13.04.qcow2
-
-glance image-create --name=ubuntu-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/ubuntu_sahara_vanilla_hadoop_1_latest.qcow2
-
-#glance image-create --name=fedora-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-0.3-vanilla-1.2.1-fedora-19.qcow2
-
-glance image-create --name=new-fedora-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/fedora_sahara_vanilla_hadoop_1_latest.qcow2
-
-#glance image-create --name=fedora-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/savanna-icehouse-vanilla-1.2.1-fedora-19.qcow2
-
-#glance image-create --name=rhel-server.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/rhel-server-x86_64-kvm-6.4_20130130.0-4.qcow2
-
-# I also open up the firewall on my local box that is running openstack (because I run dashboard/savanna from another box)
-sudo firewall-cmd --add-port=1-65535/tcp || echo "already all set"
+#glance image-create --name=ubuntu-spark.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/ubuntu_sahara_spark_latest.qcow2
+#glance image-create --name=new-fedora-oozie-hdp.image --disk-format=qcow2 --container-format=bare --is-public=True < ~/fedora_sahara_vanilla_hadoop_1_latest.qcow2
 
 # This adds a keypair to nova
 echo "Adding keypair to nova"
-nova --os-username=admin --os-tenant-name=admin --os-password=admin --os-auth-url=$OS_AUTH_URL keypair-add --pub_key ~/.ssh/id_rsa.pub stackboxkp
+#openstack keypair create stackboxkp --public-key ~/.ssh/id_rsa.pub
+nova keypair-add --pub_key ~/.ssh/id_rsa.pub stackboxkp
 
-export OS_USERNAME=admin
-keystone service-create --name sahara --type data_processing --description "Sahara Data Processing"
-keystone endpoint-create --service sahara --region RegionOne --publicurl "http://127.0.0.1:18080/v1.1/\$(tenant_id)s" --adminurl "http://127.0.0.1:18080/v1.1/\$(tenant_id)s" --internalurl "http://127.0.0.1:18080/v1.1/\$(tenant_id)s"
+export MYHOST=localhost
+keystone service-create --name sahara --type data-processing --description "Sahara Data Processing"
+keystone endpoint-create --service sahara --region RegionOne --publicurl "http://$MYHOST:18080/v1.1/\$(tenant_id)s" --adminurl "http://$MYHOST:18080/v1.1/\$(tenant_id)s" --internalurl "http://$MYHOST:18080/v1.1/\$(tenant_id)s"
 
 #create sahara-proxy domain
-~/devstack/domain.sh
+#python ~/devstack/domain.sh
+
+source ~/src/devstack/openrc demo demo
+#openstack keypair create stackboxkp --public-key ~/.ssh/id_rsa.pub
+nova keypair-add --pub_key ~/.ssh/id_rsa.pub stackboxkp
